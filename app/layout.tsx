cedef6425077bdb36339612849d57f45dbd55172
Onyeka <onyekanene@gmail.com>
2026-04-29 16:29:54 +0100

    Remove unused Analytics import from layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Syne, Space_Mono } from "next/font/google";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/next";
 
 const syne = Syne({
   subsets: ["latin"],
